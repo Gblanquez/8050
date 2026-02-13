0ff7622de3c946f86a41984d27cc9dd0bcaa74fc
Gblanquez <hellogabrielblanquez@gmail.com>
2026-02-13 11:59:37 +0100

    intro text change

--- src/animations/textIntro.js
+++ src/animations/textIntro.js
@@ -8,19 +8,20 @@ export function textIntro() {
   document.querySelectorAll('[data-a="title"]').forEach((el) => {
     gsap.set(el, { opacity: 1 })
     SplitText.create(el, {
-      type: "lines",
+      type: "lines,words,chars",
       autoSplit: true,
+      // mask: "lines",
       onSplit: (self) => {
-        gsap.set(self.lines, { x: '-10%', y: '110%', rotateX: 72, rotateY: 45, rotateZ: 4, opacity: 0 })
-        return gsap.to(self.lines, {
+        gsap.set(self.chars, { x: '-50%', y: '110%', rotateX: 72, rotateY: 45, rotateZ: 4, opacity: 0 })
+        return gsap.to(self.chars, {
           x: '0%',
           y: '0%',
           rotateX: 0,
           rotateY: 0,
           rotateZ: 0,
           opacity: 1,
-          duration: 1.6,
-          ease: 'power4.inOut',
+          duration: 2.2,
+          ease: 'power3.out',
           stagger: { each: 0.03 }
         })
       }
@@ -39,7 +40,7 @@ export function textIntro() {
         return gsap.to(self.lines, {
           y: "0%",
           stagger: 0.03,
-          duration: 1.0,
+          duration: 1.8,
           ease: "power3.out"
         })
       }
@@ -48,13 +49,13 @@ export function textIntro() {
 
   // Logo — y:110% + opacity:0 → y:0% + opacity:1
   gsap.fromTo('[data-a="logo"]',
-    { y: "110%", opacity: 0 },
-    { y: "0%", opacity: 1, duration: 1.0, ease: "power3.out" }
+    {  opacity: 0 },
+    { opacity: 1, duration: 1.6, ease: "none" }
   )
 
   // Project lines — scaleX:0 → 1, origin right
   gsap.fromTo(".project-line",
     { scaleX: 0, opacity: 0, transformOrigin: "left center" },
-    { scaleX: 1, opacity: 1, stagger: 0.04, duration: 1.2, ease: "power3.out" }
+    { scaleX: 1, opacity: 1, stagger: 0.04, duration: 1.8, ease: "power3.out" }
   )
 }
